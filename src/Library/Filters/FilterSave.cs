0b27e48c9a2b78822ff8c6d84244b379fa2bdd7b
Juan <yogursio@gmail.com>
2021-10-20 16:02:45 -0300

    new file:   src/Library/Filters/FilterSave.cs 	modified:   src/Program/Program.cs

---
+++ src/Library/Filters/FilterSave.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace CompAndDel.Filters
+{
+    /// <summary>
+    /// Filtro que guarda una imagen sin modificarla.
+    /// </remarks>
+    public class FilterSave : IFilter
+    {
+        //Creo un contador para aplicarselo despues al PictureSave de PictureProvider
+        //-----------------------------------------------------------------------------
+        public static int contador = 0; 
+        
+        public static int Contador 
+        {
+            get
+            {
+                contador += 1;
+                return contador;
+            }
+        }
+        //-----------------------------------------------------------------------------
+        /// Filtro que guarda una imagen sin modificarla.
+        /// </summary>
+        /// <param name="image">La imagen a guardar.</param>
+        /// <returns>La imagen recibida.</returns>
+        public IPicture Filter(IPicture image)
+        {
+            PictureProvider MidProvider = new PictureProvider();
+            MidProvider.SavePicture(image, @".\fotocon" + Contador + "filtro.jpg" );
+            return image;
+        }
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,24 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@".\beer.jpg");
+
+            IFilter fn = new FilterNegative();  //filtros
+            IFilter gs = new FilterGreyscale(); //filtros
+            IFilter fs = new FilterSave();      //filtros
+
+            IPipe pn = new PipeNull();
+            
+            IPipe ps = new PipeSerial(fn, pn);
+            
+            IPipe save1 = new PipeSerial(fs, pn);
+            IPipe pf = new PipeFork(ps, save1);
+            IPipe psi = new PipeSerial(gs, pf);
+
+            IPicture image = psi.Send(picture);
+            provider.SavePicture(image, @".\mmmcerveza.jpg");
+
         }
     }
 }
